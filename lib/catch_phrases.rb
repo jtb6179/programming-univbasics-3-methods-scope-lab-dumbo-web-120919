0a2c56476f6466d025dccfa96ec3fa94354f6f47
super-model-4696 <jtb6179@g.rit.edu>
2019-11-25 04:13:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,7 +4,8 @@ def mario
   puts phrase
 end
 
-def toadstoolstatus = 'Thank You Mario! But Our Princess Is In Another Castle!'
+def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
